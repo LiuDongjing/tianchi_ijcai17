75e1769e5fbf682977cb0eb6a2c3e506f3880ba5
LiuYangkai <LiuYangkai>
2017-03-03 09:58:20 +0800

    训练的框架完成了

--- script/features.py
+++ script/features.py
@@ -6,8 +6,7 @@ Created on Sat Feb 25 15:20:03 2017
 """
 import os, logging
 import pandas as pd
-from datetime import datetime
-from datetime import timedelta
+import numpy as np
 def crossJoin(df1, df2):
     '''两个DataFrame的笛卡尔积'''
     df1['_temp_key'] = 1
@@ -16,20 +15,46 @@ def crossJoin(df1, df2):
     df = df.drop('_temp_key', axis='columns')
     return df
 def getLabels(dat, mode='train'):
-    '''获取真实数据'''
-    date_list = list(pd.date_range('2015-07-08', '2016-10-31').strftime('%Y-%m-%d'))
-    date_list.remove('2015-12-12')
-    sid_pf = pd.DataFrame({'sid':[str(k) for k in range(1, 2001)]})
-    stamp_pf = pd.DataFrame({'stamp':date_list})
-    days = crossJoin(sid_pf, stamp_pf)
-    dat = dat[(dat['stamp'] >= '2015-07-08') & 
+    '''获取未来14天的销量'''
+    dat = dat[(dat['stamp'] >= '2015-07-01') & 
               (dat['stamp'] <= '2016-10-31')]
     dat['stamp'] = dat['stamp'].str[:10]
-    dat = dat.groupby(['sid', 'stamp']).size().reset_index()
-    dat.rename_axis({0:'sales'}, axis='columns', inplace=True)
-    dat = dat.astype({'sid':str, 'stamp':str, 'sales':int})
-    days = days.merge(dat, how='left', on=['sid', 'stamp'])
-    days['sales'].fillna(0, inplace=True)
+    dat = dat.groupby('sid')
+    days = None
+    for sid in [str(k) for k in range(1, 2001)]:
+        shop = dat.get_group(sid)
+        logging.info('getLabels:%s.' % sid)
+        shop = shop.drop('sid', axis='columns')
+        shop = shop.groupby('stamp').size().reset_index()
+        shop.rename_axis({0:'sales'}, axis='columns', inplace=True)
+        shop = shop.sort_values('stamp')
+        N = shop.shape[0]
+        if N < 14:
+            logging.warn('%s的数据条数不足14个.'%sid)
+            continue
+        full = pd.DataFrame({'stamp':
+                    pd.date_range(shop['stamp'][0], '2016-10-31').\
+                                 strftime('%Y-%m-%d')})
+        shop = full.merge(shop, how='left', on='stamp')
+        shop.fillna(0, inplace=True)
+        idx = shop[shop['stamp'] == '2015-12-12'].axes[0]
+        if len(idx) >= 1:
+            if idx[0] > 0:
+                shop.loc[idx, 'sales'] = round((shop.loc[idx-1, 'sales'].\
+                        values[0] + shop.loc[idx+1, 'sales'].values[0])/2)
+            else:
+                shop.loc[idx, 'sales'] = shop.loc[idx+1, 'sales'].values[0]
+        #前7天用于提取特征
+        temp = pd.DataFrame({'stamp':shop['stamp'][7:-14].reset_index(drop=True)})
+        N = shop.shape[0]
+        for n in range(14):
+            t = shop['sales'][7+n:N+n-14].reset_index(drop=True)
+            temp.insert(n+1, 'day%d'%(n+1), t)
+        temp['sid'] = sid
+        if days is None:
+            days = temp
+        else:
+            days = days.append(temp)
     return days
 def extractAll(mode = 'train'):
     featurePath = os.path.join('../temp/', mode + '_features.csv')
@@ -43,7 +68,8 @@ def extractAll(mode = 'train'):
     #提取特征
     logging.info('加载user_pay.txt...')
     user_pay = pd.read_csv('../input/dataset/user_pay.txt', 
-                           header = None, names = ['uid', 'sid', 'stamp'])
+                           header = None, names = ['uid', 'sid', 'stamp'],
+                           dtype = np.str)
     logging.info('提取Label...')
     labels = getLabels(user_pay)
     f1 = Last_week_sales(mode=mode)
@@ -54,7 +80,7 @@ def extractAll(mode = 'train'):
        not (features['stamp'].equals(labels['stamp'])):
         features.to_csv(featurePath+'.dump', index=False, encoding='utf-8')
         labels.to_csv(labelPath+'.dump', index=False, encoding='utf-8')
-        raise '特征和标签不匹配！数据已保存到dump。'
+        raise Exception('特征和标签不匹配！数据已保存到dump。')
     #保存计算的features到outPath
     features = features.drop(['sid', 'stamp'], axis='columns')
     labels = labels.drop(['sid', 'stamp'], axis='columns')
@@ -71,7 +97,7 @@ class BaseFeature:
         self.name = featureName
         self.data = None
         if os.path.exists(self.outFile):
-            self.data = pd.read_csv(self.outFile)
+            self.data = pd.read_csv(self.outFile, dtype = np.str)
             logging.info('从%s中载入特征%s.' % (self.outFile, self.name))
     def extract(self, indata):
         return self.data
@@ -89,37 +115,43 @@ class Last_week_sales(BaseFeature):
         #提取特征
         dat = indata
         dat['stamp'] = dat['stamp'].str[:10]
-        dat = dat.groupby(['sid', 'stamp']).size().reset_index()
-        #生成格式为sid, stamp的DataFrame；其中sid取值[1, 2000]，
-        #stamp是[2015-07-08, 2016-10-31]。最后生成的DF是两者的笛卡尔积
-        date_list = list(pd.date_range('2015-07-08', '2016-10-31').strftime('%Y-%m-%d'))
-        date_list.remove('2015-12-12')
-        sid_pf = pd.DataFrame({'sid':[str(k) for k in range(1, 2001)]})
-        stamp_pf = pd.DataFrame({'stamp':date_list})
-        days = crossJoin(sid_pf, stamp_pf)
-        #
-        dat.rename_axis({0:'sales'}, axis='columns', inplace=True)
-        dat = dat.sort_values(['sid', 'stamp'])
-        dat = dat.astype({'sid':str, 'stamp':str, 'sales':int})
-        applyFunc = lambda k:\
-            lambda s:(datetime.strptime(s, '%Y-%m-%d') + 
-                      timedelta(days=k+1)).\
-                      strftime('%Y-%m-%d') \
-                      if '2015-12-05' <= s and s <= '2015-12-12'\
-                      else\
-                      (datetime.strptime(s, '%Y-%m-%d') + 
-                      timedelta(days=k)).strftime('%Y-%m-%d')
-        
-        for k in range(1, 8):
-            day = dat[(dat['stamp'] >= ('2015-07-0%d' % k)) & 
-                      (dat['stamp'] <= ('2016-10-%d' % (23+k)))]
-            timeShift = day.stamp.apply(applyFunc(8-k))
-            day = day.drop('stamp', axis='columns')
-            day = day.rename_axis({'sales':'day%d'%k}, axis='columns')
-            day.insert(1, 'stamp', timeShift)
-            days = days.merge(day, how='left', on=['sid', 'stamp'])
-            days['day%d'%k].fillna(0, inplace=True)
-            logging.info('已提取第%d的销量.'%k)
+        dat = dat[(dat['stamp'] >= '2015-07-01') & 
+          (dat['stamp'] <= '2016-10-17')]
+        dat = dat.groupby('sid')
+        days = None
+        for sid in [str(k) for k in range(1, 2001)]:
+            shop = dat.get_group(sid)
+            logging.info('last_week_sales:%s.' % sid)
+            shop = shop.drop('sid', axis='columns')
+            shop = shop.groupby('stamp').size().reset_index()
+            shop.rename_axis({0:'sales'}, axis='columns', inplace=True)
+            shop = shop.sort_values('stamp')
+            N = shop.shape[0]
+            if N < 7:
+                logging.warn('%s的数据条数不足7个.'%sid)
+                continue
+            full = pd.DataFrame({'stamp':
+                        pd.date_range(shop['stamp'][0], '2016-10-17').\
+                                     strftime('%Y-%m-%d')})
+            shop = full.merge(shop, how='left', on='stamp')
+            shop.fillna(0, inplace=True)
+            idx = shop[shop['stamp'] == '2015-12-12'].axes[0]
+            if len(idx) >= 1:
+                if idx[0] > 0:
+                    shop.loc[idx, 'sales'] = round((shop.loc[idx-1, 'sales'].\
+                            values[0] + shop.loc[idx+1, 'sales'].values[0])/2)
+                else:
+                    shop.loc[idx, 'sales'] = shop.loc[idx+1, 'sales'].values[0]
+            temp = pd.DataFrame({'stamp':shop['stamp'][7:].reset_index(drop=True)})
+            N = shop.shape[0]
+            for n in range(7):
+                t = shop['sales'][n:N-7+n].reset_index(drop=True)
+                temp.insert(n+1, 'day%d'%(n+1), t)
+            temp['sid'] = sid
+            if days is None:
+                days = temp
+            else:
+                days = days.append(temp)
         days.to_csv(self.outFile, index=False, encoding='utf-8', 
                           float_format='%0.0f')
         logging.info('已将最近7天的销售数据保存到%s.'%self.outFile)
--- script/main.py
+++ script/main.py
@@ -10,6 +10,45 @@ import dataproc
 from time import clock
 from sklearn.externals import joblib
 from sklearn.model_selection import KFold, cross_val_score
+import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import LinearRegression
+def official_loss(estimator, X, y):
+    #注意重置index，不然会出现意想不到的问题
+    y_ = y.reset_index(drop=True)
+    y_p = estimator.predict(X)
+    adds = (y_p + y_).abs()
+    subs = (y_p - y_).abs()
+    divs = subs / adds
+    N = divs.shape[0] * divs.shape[1]
+    return divs.sum().sum() / N
+class Warpxgboost(BaseEstimator):
+    ''''''
+    def __init__(self, model):
+        self.model = model
+    def fit(self, X, y):
+        '''X=[n_samples, n_features]
+           y=[n_samples, n_targets]'''
+        #注意重置index，不然模型计算过程中会出错
+        X_ = X.reset_index(drop=True)
+        y_ = y.iloc[:, 0].reset_index(drop=True)
+        self.model.fit(X_, y_)
+        return self
+    def predict(self, X):
+        '''X=[n_samples, n_features]'''
+        #注意重置index，不然模型计算过程中会出错
+        dat = X.reset_index(drop=True)
+        labels = pd.DataFrame()
+        for k in range(14):
+            p = self.model.predict(dat)
+            p = pd.DataFrame({'tmp%d'%k:p})
+            labels.insert(k, 'day%d'%(k+1), p)
+            dat = dat.drop(dat.columns[0], axis='columns')
+            dat.insert(len(dat.columns), '_%d'%k, p)
+            dat.columns = ['day%d'%n for n in range(1, 8)]
+        labels.columns = ['day%d'%k for k in range(1, 15)]
+        return labels#array, (n_samples,)
 def main():
     model = None
     if os.path.exists('../temp/model.pkl'):
@@ -18,21 +57,26 @@ def main():
         dataproc.preprocess()
         (feature, label) = extractAll()
         logging.info('共有%d条训练数据.' % feature.shape[0])
-        index = (feature['day1'] != 0) |\
-                (feature['day2'] != 0) |\
-                (feature['day3'] != 0) |\
-                (feature['day4'] != 0) |\
-                (feature['day5'] != 0) |\
-                (feature['day6'] != 0) |\
-                (feature['day7'] != 0)
+        index = (feature['day1'] > 0) |\
+                (feature['day2'] > 0) |\
+                (feature['day3'] > 0) |\
+                (feature['day4'] > 0) |\
+                (feature['day5'] > 0) |\
+                (feature['day6'] > 0) |\
+                (feature['day7'] > 0)
+        for k in range(14):
+            index = index | label['day%d'%(k+1)] > 0
         feature = feature[index]
         label = label[index]
         logging.info('去掉无效数据后还剩%d条.' % (feature.shape[0]))
+        model = xgboost.XGBRegressor(silent=True,  n_estimators=100)
+        #model = LinearRegression()
         logging.info('开始交叉验证...')
-        model = xgboost.XGBRegressor(silent=True)
-        scores = cross_val_score(model, feature, label, 
-                                 cv=KFold(n_splits=3), 
-                                 n_jobs=-1)
+        #注意n_jobs使用多CPU时，不可以调试，否则会抛出异常
+        scores = cross_val_score(Warpxgboost(model), feature, label, 
+                                 cv=KFold(n_splits=3, shuffle=True), 
+                                 n_jobs=-1, 
+                                 scoring=official_loss)
         logging.info('交叉验证的结果%s' % str(scores))
 #
 if __name__ == '__main__':
