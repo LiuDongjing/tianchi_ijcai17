993f9feab21b49ee7414b9f23f7618b7268775d6
LiuYangkai <LiuYangkai>
2017-03-09 15:34:17 +0800

    将xgboost、gbdt和randomforest融合在一起

--- script/main.py
+++ script/main.py
@@ -13,7 +13,26 @@ from sklearn.model_selection import KFold, cross_val_score
 import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from random import randint
+def blend(lst, w):
+    '''将多个结果根据权重融合起来'''
+    r = lst[0] * w[0]
+    for k in range(1, len(lst)):
+        r = r + lst[k]*w[k]
+    return r.round()
+def select_test(n, count):
+    '''从大小为n的样本中随机选择count个作为测试，其余的用来训练'''
+    index = [False for _ in range(n)]
+    p = 0
+    while p < count:
+        ii = randint(0, n-1)
+        if index[ii]:
+            continue
+        index[ii] = True
+        p += 1
+    return index
 def official_loss(estimator, X, y):
     #注意重置index，不然会出现意想不到的问题
     y_ = y.reset_index(drop=True)
@@ -66,15 +85,22 @@ def main():
     model = None
     #mode = 'predict'
     modelPath = '../temp/model.pkl'
+    modelWeight = [0.3, 0.4, 0.3]
+    modelName = ['xgboost', 'GBDT', 'RandomForest']
     if os.path.exists(modelPath):
         logging.info('从%s中加载模型...'%modelPath)
         #joblib.load加载的是保存到磁盘中的对象，不仅仅是训练好的模型
-        model = joblib.load(modelPath) 
+        modelList = joblib.load(modelPath) 
         feature = extractAll('predict')
         X = feature.iloc[:, 1:]
-        logging.info('预测中...')
-        y = model.predict(X)
-        logging.info('预测结束.')
+        resList = []
+        for k in range(len(modelName)):
+            model = modelList[k]
+            logging.info('%s:预测中...'%modelName[k])
+            resList.append(model.predict(X))
+            logging.info('%s:预测结束.'%modelName[k])
+        logging.info('融合模型...')
+        y = blend(resList, modelWeight)
         y.insert(0, 'sid', feature['sid'])
         y.to_csv('../temp/result.csv', header=False, index=False,
                  encoding='utf-8', float_format='%0.0f')
@@ -92,20 +118,37 @@ def main():
         feature = feature[index]
         label = label[index]
         logging.info('去掉无效数据后还剩%d条.' % (feature.shape[0]))
-        model = WarpModel(xgboost.XGBRegressor(
-                          silent=True, n_estimators=100))
-        #model = WarpModel(LinearRegression)
-        logging.info('开始交叉验证...')
-        #注意n_jobs使用多CPU时，不可以调试，否则会抛出异常
-        scores = cross_val_score(model, feature, label, 
-                                 cv=KFold(n_splits=3, shuffle=True), 
-                                 #n_jobs=-1, 
-                                 scoring=official_loss
-                                 )
-        logging.info('交叉验证的结果%s' % str(scores))
-        logging.info('用所有数据训练模型...')
-        model.fit(feature, label)
-        joblib.dump(model, modelPath)
+        test_set = select_test(feature.shape[0], round(feature.shape[0]*0.1))
+        test_feature = feature.loc[test_set, :]
+        test_label = label.loc[test_set, :]
+        logging.info('用%d个样本用作最终的测试.'%test_feature.shape[0])
+        for k in range(len(test_set)):
+            test_set[k] = not test_set[k]
+        feature = feature.loc[test_set, :]
+        label = label.loc[test_set, :]
+        logging.info('用%d个样本用作训练.'%feature.shape[0])
+        modelList = [WarpModel(xgboost.XGBRegressor(
+                          silent=True, n_estimators=100)), 
+                     WarpModel(GradientBoostingRegressor()),
+                     WarpModel(RandomForestRegressor())]
+        
+        for k in range(len(modelList)):
+            logging.info('%s: 交叉验证...'%modelName[k])
+            model = modelList[k]
+            #注意n_jobs使用多CPU时，不可以调试，否则会抛出异常
+            scores = cross_val_score(model, feature, label, 
+                                     cv=KFold(n_splits=3, shuffle=False), 
+                                     #n_jobs=-1, 
+                                     scoring=official_loss
+                                     )
+            logging.info('交叉验证结果：%s' % str(scores))
+            logging.info('用所有的训练数据训练模型...')
+            model.fit(feature, label)
+            modelList[k] = model
+            logging.info('%s测试模型...' % modelName[k])
+            logging.info('测试结果: %f' % official_loss(model, test_feature, \
+                                                    test_label))
+        joblib.dump(modelList, modelPath)
         logging.info('已将训练好的模型保存到%s.'%modelPath)
 #
 if __name__ == '__main__':
